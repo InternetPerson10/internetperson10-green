8a5b5ce2c0a41a20a798cb4c266932e02b6b992c
InternetPerson10 <58873475+InternetPerson10@users.noreply.github.com>
2021-08-24 17:45:13 +0800

    Update green.py

--- green.py
+++ green.py
@@ -9,6 +9,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix = "green!")
 
 handles_track = {}
+handles_contest = {}
 
 @bot.command()
 async def test(ctx):
@@ -57,31 +58,33 @@ async def latest(ctx, handle, contest=False):
 
     last_sub_id[handle] = sub["id"]
 
+    print("New sub");
+
     if(sub["verdict"] == "OK"):
         if contest == True:
-            await ctx.send("Accepted! :D " + 
+            await ctx.message.author.send("Accepted! :DD " + 
                 str(sub["problem"]["contestId"]) +
                 sub["problem"]["index"] + ", " +
                 str(sub["timeConsumedMillis"]) + "ms, " + 
                 str(sub["memoryConsumedBytes"]/1000) + "KB", tts = True)
-        else:
+        if handle in handles_track:
             await ctx.send(sub["author"]["members"][0]["handle"] +
                 " got ACCEPTED on problem " +
                 str(sub["problem"]["contestId"]) +
                 sub["problem"]["index"] +
                 " (" +
                 sub["problem"]["name"] +
-                ")!")
+                ")! <:ginkobingo:859811367556612126>")
 
     else:
         if contest == True:
-                await ctx.send((sub["verdict"].replace("_", " ")).capitalize() +
+            await ctx.message.author.send((sub["verdict"].replace("_", " ")).capitalize() +
                 " on test " + str(sub["passedTestCount"]+1) + ": " +
                 str(sub["problem"]["contestId"]) +
                 sub["problem"]["index"] + ", " +
                 str(sub["timeConsumedMillis"]) + "ms, " + 
                 str(sub["memoryConsumedBytes"]//1000) + "KB", tts = True)
-        else:
+        if handle in handles_track:
             await ctx.send(sub["author"]["members"][0]["handle"] +
                 " got " +
                 sub["verdict"].replace("_", " ") +
@@ -95,8 +98,11 @@ async def latest(ctx, handle, contest=False):
 TRACK_TIME = 7200
 LIMIT = 7
 
-@bot.command(brief = "Track someone on CF", description = "Alerts you whenever a submission has finished judging on Codeforces.\nBy default, stops tracking after one hour, but this can be changed.\nUse in DM to enable SERIOUS MODE! Great for virtuals/contests, it features less delay, less clutter, and more useful info.")
+@bot.command(brief = "Track someone on CF", description = "Alerts the channel whenever a submission has finished judging on Codeforces.\nBy default, stops tracking after one hour, but this can be changed.\nUse in DM to enable SERIOUS MODE! Great for virtuals/contests, it features less delay, less clutter, and more useful info.")
 async def track(ctx, handle, track_time = 60):
+
+    handle = str(handle)
+    handle = handle.lower()
 
     # error handling
     if not isinstance(track_time, int):
@@ -113,7 +119,7 @@ async def track(ctx, handle, track_time = 60):
         return
 
     # too long
-    if track_time >= 720:
+    if track_time >= 360:
         await ctx.send("Track time is very long! Note that after a restart, all tracks will be lost.")
     
     # mark handle with end time
@@ -124,21 +130,13 @@ async def track(ctx, handle, track_time = 60):
     # send
     await ctx.send("Now tracking " + handle + " :eyes:")
 
-    contest = False
-
-    if isinstance(ctx.channel, discord.channel.DMChannel):
-        contest = True
-
     # loop until either untrack or time end
     while (handle in handles_track) and (time.time() < end_time):
-        res = await latest(ctx, handle, contest)
+        res = await latest(ctx, handle)
         if res == False:
             handles_track.discard(handle)
-            break
-        if isinstance(ctx.channel, discord.channel.DMChannel):
-            await asyncio.sleep(4)
-        else:
-            await asyncio.sleep(15)
+            break;
+        await asyncio.sleep(15)
 
     # check if time too long
     if time.time() >= end_time:
@@ -147,6 +145,8 @@ async def track(ctx, handle, track_time = 60):
 
 @bot.command(brief = "Stop tracking someone")
 async def untrack(ctx, handle):
+    handle = str(handle)
+    handle = handle.lower()
     if handle not in handles_track:
         await ctx.send(handle + " not being tracked right now")
         return
@@ -163,6 +163,61 @@ async def tracklist(ctx):
         left_time = handles_track[handle] - time.time()
         s = s + "\n" + handle + ": " + str(int(left_time // 60)) + "m" + str(int(left_time % 60)) + "s left"
     await ctx.send(s)
+
+@bot.command(brief = "Serious tracking for contests/virtuals")
+async def contest(ctx, handle, track_time = 60):
+
+    handle = str(handle)
+    handle = handle.lower()
+
+    # error handling
+    if not isinstance(track_time, int):
+        await ctx.send("Track length must be an integer oops")
+        return
+    if len(handles_contest) >= LIMIT:
+        await ctx.send("Woah hold on I can only track " + str(LIMIT) + " ppl at a time, I'm not a good stalker yet.")
+        return
+    if handle in handles_contest:
+        await ctx.send("Already tracking " + handle)
+        return
+    res = await latest(ctx, handle)
+    if res == False:
+        return
+
+    # too long
+    if track_time >= 360:
+        await ctx.send("Track time is very long! Note that after a restart, all tracks will be lost.")
+    
+    # mark handle with end time
+    start_time = time.time()
+    end_time = start_time + 60 * track_time
+    handles_contest[handle] = end_time
+
+    # send
+    await ctx.message.author.send(":thumbsup: Will DM you once a submission has been judged. Thanks for using Green! :D")
+
+    # loop until either untrack or time end
+    while (handle in handles_contest) and (time.time() < end_time):
+        res = await latest(ctx, handle, True)
+        if res == False:
+            handles_contest.discard(handle)
+            break;
+        await asyncio.sleep(4)
+
+    # check if time too long
+    if time.time() >= end_time:
+        await ctx.send(str(track_time) + " minutes have passed, now untracking " + handle)
+        handles_contest.pop(handle)
+
+@bot.command(brief = "Stop tracking someone in contest mode")
+async def uncontest(ctx, handle):
+    handle = str(handle)
+    handle = handle.lower()
+    if handle not in handles_contest:
+        await ctx.send(handle + " not being tracked right now")
+        return
+    handles_contest.pop(handle)
+    await ctx.send("Stopped tracking " + handle)
 
 @bot.event
 async def on_ready():
